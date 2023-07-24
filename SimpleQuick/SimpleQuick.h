2bc670b3ba4d39bdb431b9c52b7a1e13d72aa634
Phil Chang <phil.chang@yahooinc.com>
2023-07-24 17:29:07 +0800

    Initial Commit

---
+++ SimpleQuick/SimpleQuick.h
@@ -0,0 +1,19 @@
+//
+//  SimpleQuick.h
+//  SimpleQuick
+//
+//  Created by Phil Chang on 2023/7/24.
+//  Copyright © 2023 Yahoo. All rights reserved.
+//        
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleQuick.
+FOUNDATION_EXPORT double SimpleQuickVersionNumber;
+
+//! Project version string for SimpleQuick.
+FOUNDATION_EXPORT const unsigned char SimpleQuickVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleQuick/PublicHeader.h>
+
+
